a7e3827d295ec0208d2a5a6eefc94f46728034ec
T.Ruchitha <thumuruchi@gmail.com>
2026-08-10 18:47:26 +0530

    Add Student class with a constructor

---
+++ constructor.java
@@ -0,0 +1,14 @@
+class Student
+{
+    Student(){
+    
+		System.out.println("Constructor called");
+	}
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Student s=new Student();
+    }
+    
+}
